af9661017fcd3dea5c6f88c4e727b9c59b69b9e1
Joy <joykarmakar987654321@gmail.com>
2025-08-05 21:23:39 +0530

    update

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,14 +3,17 @@ import React from "react";
 
 const page = () => {
     return (
-        <div>
-            <Image src="/img.jpg" alt="" height={200} width={300} />
-            <Image
-                src="https://images.unsplash.com/photo-1740507619572-ac180ca2630f?q=80&w=1487&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt=""
-                height={200}
-                width={300}
-            />
+        <div className="container mx-auto px-10">
+            <div className="flex flex-col gap-5">
+                <Image src="/img.jpg" alt="" height={768} width={1200} />
+
+                <Image
+                    src="https://images.unsplash.com/photo-1740507619572-ac180ca2630f?q=80&w=1487&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    alt=""
+                    height={768}
+                    width={1200}
+                />
+            </div>
         </div>
     );
 };
